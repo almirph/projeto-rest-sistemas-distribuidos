78d217e07b7e716dcf21a98152bdc75c2978f8a6
almir.arruda <almir.arruda@ntendencia.com.br>
2020-11-27 14:07:43 -0300

    projeto

---
+++ server.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const requireDir = require("require-dir");
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+//Iniciando o DB
+mongoose.connect("mongodb://localhost:27017/nodeapi", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+requireDir("./src/models");
+
+//Primeira rota
+app.use("/api", require("./src/routes"));
+
+app.listen(3001);
